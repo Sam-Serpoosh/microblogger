e33f8a9d2d4953f3115f990f81a1dd35aafa571e
Sam Serpoosh <ssjesus287@gmail.com>
2013-01-28 08:50:15 -0500

    getting command from user and tweeting

--- microblogger.rb
+++ microblogger.rb
@@ -3,8 +3,30 @@ require 'jumpstart_auth'
 class MicroBlogger
   attr_reader :client 
   def initialize(twitter_client)
-    puts "initializing"
     @client = twitter_client
+  end
+
+  def run
+    puts "welcome to the twitter client app!"
+    command = ""
+    while command != "q"
+      printf "enter a command: "
+      input = gets.chomp
+      command = extract_command(input)
+      parameter = extract_parameter(input)
+      process_command(command, parameter)
+    end
+  end
+
+  def process_command(command, parameter)
+      case command
+        when "q"
+          puts "Goodbye!"
+        when "t"
+          tweet(parameter)
+        else
+          puts "Sorry, I don't know how to #{command}"
+      end
   end
 
   def tweet(message)
@@ -14,6 +36,16 @@ class MicroBlogger
       puts "Your message can't be more than 140 chars!!!should "
     end
   end
+
+  def extract_command(input)
+    input.split(" ")[0]
+  end
+
+  def extract_parameter(input)
+    parts = input.split(" ")
+    parts[1..-1].join(" ")
+  end
 end
 
-#blogger = MicroBlogger.new(JumpstartAuth.twitter)
+blogger = MicroBlogger.new(JumpstartAuth.twitter)
+blogger.run
--- microblogger_spec.rb
+++ microblogger_spec.rb
@@ -15,4 +15,17 @@ describe MicroBlogger do
     twitter_client.should_not_receive(:update)
     blogger.tweet(long_message)
   end
+
+  it "extract command part out of the input" do
+    input = "t this is a test tweet"
+    command = blogger.extract_command(input)
+    command.should == "t"
+  end
+
+  it "extracts the parameter out of the input" do
+    tweet = "this is a test tweet"
+    input = "t #{tweet}"
+    parameter = blogger.extract_parameter(input) 
+    parameter.should == tweet
+  end
 end
